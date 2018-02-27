ef62c69cfaa659f5436a02c76f9b5b5c14e94fd8
Lucas Azzola <derflatulator@gmail.com>
2018-02-27 22:00:02 +1100

    .

---
+++ README.md
@@ -0,0 +1,4 @@
+# Flag of the Day
+
+View today's flag at https://dailyflag.now.sh/, or go to a particular flag by the country code,
+e.g: https://dailyflag.now.sh/au.
---
+++ index.js
@@ -0,0 +1,82 @@
+const fs = require("fs");
+const util = require("util");
+const readFile = util.promisify(fs.readFile);
+const countries = require("svg-country-flags/countries");
+const codes = Object.keys(countries);
+const { send } = require("micro");
+
+const CDN =
+  "https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.0.0/flags/4x3/";
+
+const styles = `
+  <style>
+  body {
+    background: lightgrey;
+  }
+  h1 {
+    font-family: Candara, Calibri, Segoe, "Segoe UI", Optima, Arial, sans-serif;
+    margin: 0;
+    text-align: center;
+  }
+  </style>  
+`;
+
+module.exports = async (req, res) => {
+  const days = msToDay(Date.now());
+  const match = req.url.match(/^\/(\w{2})$/);
+  const codeIsTodays = !match;
+  const code = codeIsTodays
+    ? codes[days % codes.length]
+    : match[1].toUpperCase();
+  const name = countries[code];
+
+  if (!name) {
+    return send(
+      res,
+      404,
+      `
+      <!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <title>404 - 🏴 Not Found</title>
+          ${styles}
+        </head>
+      </html>
+      <h1>404 - 🏴 Not Found</h1>
+      `
+    );
+  }
+
+  const title = `${codeIsTodays ? "🏴 of the day: " : ""} ${name}`;
+
+  return `
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      ${styles}
+      <style>
+      .Container {
+        display: flex;
+        flex-direction: column;
+        height: 100vh;
+      }
+      .Flag {
+        flex-grow: 1;
+      }
+      .Title {
+        padding: 0.5em;
+      }
+      </style>
+      <title>${title}</title>
+    </head>
+    
+    <div class=Container>
+      <img class=Flag src="${CDN}${code.toLowerCase()}.svg" style="object-fit: contain;">
+      <h1 class=Title>${title}</h1>
+    </div>
+  `;
+};
+
+const msToDay = ms => Math.floor(ms / 1000 / 60 / 60 / 24);
